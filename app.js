d1c329f13f207c695707eb4170633895d84c5cd9
Sayan4562 <97144518+Sayan4562@users.noreply.github.com>
2025-07-09 20:46:15 +0530

    second adition

---
+++ app.js
@@ -0,0 +1,10 @@
+
+function open_manu(){
+    const manu= document.querySelector(".nav-sidebar")
+    manu.classList.add('show-manu')
+}
+
+function close_manu(){
+    const manu= document.querySelector(".nav-sidebar")
+    manu.classList.add('hide-manu')
+}
